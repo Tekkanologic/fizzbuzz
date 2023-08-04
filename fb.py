928f34c56369465738d1514b45afaf16c9199658
lukevc <lukevincentcannon@hotmail.co.uk>
2023-08-04 16:38:02 +0100

    Updated nested if statement to python10 match case

--- fb.py
+++ fb.py
@@ -2,21 +2,21 @@ import unittest
 
 
 def fizzbuzz(n, debug):
-    
     FIZZ = 3
     BUZZ = 5
     numbers = []
 
     for i in range(n):
         i += 1
-        if i % FIZZ == 0 and i % BUZZ == 0:
-            numbers.append("FIZZBUZZ")
-        elif i % FIZZ == 0:
-            numbers.append("FIZZ")
-        elif i % BUZZ == 0:
-            numbers.append("BUZZ")
-        else:
-            numbers.append(i)
+        match i:
+            case i if i % FIZZ == 0 and i % BUZZ == 0:
+                numbers.append("FIZZBUZZ")
+            case i if i % FIZZ == 0:
+                numbers.append("FIZZ")
+            case i if i % BUZZ == 0:
+                numbers.append("BUZZ")
+            case _:
+                numbers.append(i)
 
     # if not in debug mode, return the full list 
     if not debug:
